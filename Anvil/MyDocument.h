41cafc94d17ffbb481aeb335f2a183106108e1cf
McSpider <circuit.spider@gmail.com>
2012-07-01 19:28:50 -0700

    Initial Commit

---
+++ Anvil/MyDocument.h
@@ -0,0 +1,14 @@
+//
+//  MyDocument.h
+//  Anvil
+//
+//  Created by Benjamin Kohler on 12/07/01.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument {
+}
+
+@end
---
+++ Anvil/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Anvil
+//
+//  Created by Benjamin Kohler on 12/07/01.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+  return NSApplicationMain(argc, (const char **)argv);
+}
